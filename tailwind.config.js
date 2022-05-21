35f0cb0909775214bcc7fe4d134f1175b81e525f
zakzakst <e1433104@gmail.com>
2022-05-21 17:32:01 +0900

    実験

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f00',
+      },
+      backgroundImage: {
+        'primary-gradient': 'linear-gradient(to right, #f00, #fff)',
+      },
+      borderRadius: {
+        DEFAULT: '8px',
+        'lg': '24px',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
